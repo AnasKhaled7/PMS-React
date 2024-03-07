a421f1ce440fb26dbe2953c445de4da8cd055764
Mai-Fouad <maifouad136@gmail.com>
2024-03-07 05:29:38 +0200

    Login UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import ProtectedRoute from './SharedModule/components/ProtectedRoute/ProtectedRoute';
-import Home from './HomeModule/components/Home/Home';
-import NotFound from './SharedModule/components/NotFound/NotFound';
-import AuthLayout from './SharedModule/components/AuthLayout/AuthLayout';
-import Login from './AuthModule/components/Login/Login';
-import ResetPass from './AuthModule/components/ResetPass/ResetPass';
-import Register from './AuthModule/components/Register/Register';
-import VerifyAccount from './AuthModule/components/VerifyAccount/VerifyAccount';
-import ForgotPass from './AuthModule/components/ForgotPass/ForgotPass';
-import ChangePass from './AuthModule/components/ChangePass/ChangePass';
-import UserList from './UserModule/components/UserList/UserList';
-import TasksList from './TasksModule/components/TasksList/TasksList';
-import ProjectsList from './ProjectsModule/componenets/ProjectsList/ProjectsList';
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ProtectedRoute from "./SharedModule/components/ProtectedRoute/ProtectedRoute";
+import Home from "./HomeModule/components/Home/Home";
+import NotFound from "./SharedModule/components/NotFound/NotFound";
+import AuthLayout from "./SharedModule/components/AuthLayout/AuthLayout";
+import Login from "./AuthModule/components/Login/Login";
+import ResetPass from "./AuthModule/components/ResetPass/ResetPass";
+import Register from "./AuthModule/components/Register/Register";
+import VerifyAccount from "./AuthModule/components/VerifyAccount/VerifyAccount";
+import ForgotPass from "./AuthModule/components/ForgotPass/ForgotPass";
+import ChangePass from "./AuthModule/components/ChangePass/ChangePass";
+import UserList from "./UserModule/components/UserList/UserList";
+import TasksList from "./TasksModule/components/TasksList/TasksList";
+import ProjectsList from "./ProjectsModule/componenets/ProjectsList/ProjectsList";
 
 export default function App() {
 
@@ -24,7 +24,7 @@ export default function App() {
         { index: true, element: <Home /> },
         { path: "users", element: <UserList /> },
         { path: "tasks", element: <TasksList /> },
-        { path: "prjects", element: <ProjectsList /> },
+        { path: "projects", element: <ProjectsList /> },
       ],
     },
 
@@ -33,14 +33,12 @@ export default function App() {
       errorElement: <NotFound />,
       children: [
         { index: true, element: <Login /> },
-        { path: "login", element: <Login/> },
+        { path: "login", element: <Login /> },
         { path: "forgot-pass", element: <ForgotPass /> },
         { path: "reset-pass", element: <ResetPass /> },
         { path: "register", element: <Register /> },
         { path: "verification", element: <VerifyAccount /> },
         { path: "change-pass", element: <ChangePass /> },
-
-
       ],
     },
   ]);
@@ -48,7 +46,6 @@ export default function App() {
   return (
     <>
       <RouterProvider router={routes} />
-
     </>
-  )
+  );
 }
--- src/AuthModule/components/Login/Login.tsx
+++ src/AuthModule/components/Login/Login.tsx
@@ -1,7 +1,93 @@
-import React from 'react'
+import style from "./Login.module.css";
+import logo from "../../../assets/images/PMS3.png";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Login() {
+  const [showPass, setShowPass] = useState(false);
+
+  const showPassHandler = () => {
+    setShowPass(!showPass);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className={`vh-100 ${style["auth-container"]}`}>
+      <div className="container">
+        <div className="row vh-100 justify-content-center align-items-center">
+          <div className="col-md-7">
+            <div className="logo text-center mb-2">
+              <img src={logo} alt="Logo" />
+            </div>
+            <div className="form-container rounded-4 text-light p-5">
+              <div className="form-info mb-5">
+                <p className="lh-1 form-title">welcome to PMS</p>
+                <h2 className="form-header lh-1">Login</h2>
+              </div>
+              <form>
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <div className="input-group mb-4">
+                  <input
+                    id="email"
+                    type="email"
+                    className="form-control bg-transparent border-0 border-bottom rounded-0 p-0 shadow-none text-light"
+                    placeholder="Enter your E-mail"
+                  />
+                </div>
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <div className="input-group mb-4">
+                  <input
+                    id="password"
+                    type={showPass ? "password" : "text"}
+                    className="form-control bg-transparent border-0 border-bottom rounded-0 p-0 shadow-none text-light"
+                    placeholder="Enter your password"
+                  />
+                  <span className="input-group-text bg-transparent border-0 border-bottom rounded-0">
+                    {showPass ? (
+                      <i
+                        className="fa-regular fa-eye text-light"
+                        role="button"
+                        onClick={showPassHandler}
+                      ></i>
+                    ) : (
+                      <i
+                        className="fa-regular fa-eye-slash text-light"
+                        role="button"
+                        onClick={showPassHandler}
+                      ></i>
+                    )}
+                  </span>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <Link
+                    to={"register"}
+                    className="text-decoration-none text-light"
+                  >
+                    Register Now ?
+                  </Link>
+                  <Link
+                    to={"forgot-pass"}
+                    className="text-decoration-none text-light"
+                  >
+                    Forget Password ?
+                  </Link>
+                </div>
+                <div className="text-center mt-4">
+                  <button
+                    type="submit"
+                    className="btn form-btn w-75 rounded-4 text-light"
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
